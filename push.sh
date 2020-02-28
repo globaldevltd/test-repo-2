fd91fb42c0572e4ec0802b8d7099138bacf42f59
lukeda <lukeda@gmail.com>
2020-02-28 19:11:05 +1000

    fixing a really easy problem by using lowercase

--- push.sh
+++ push.sh
@@ -1,3 +1,8 @@
-MSG=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1`
-echo $MSG >> strings.txt
-git commit -am $MSG && git push
+HASH=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1`
+MSG=$HASH
+if [ "$?" -eq 0 ]
+  then
+    MSG=$1
+fi
+echo $HASH >> strings.txt
+git commit -am "$MSG" && git push
